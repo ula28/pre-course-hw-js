50d61f0e9904d95dd026214206fab587bc72ff19
ula28 <ula24499@gmail.com>
2023-07-20 16:58:27 +0200

    03-01

---
+++ lesson-01/03.js
@@ -0,0 +1,14 @@
+// Имея в распоряжении только:
+
+// ​let number = 1;
+// и используя лишь
+// --number   number--  ++number   number++
+// Получи в консоли 1,3,3,1 
+// ❗Нужно вызвать консоль ЧЕТЫРЕ раза
+
+let number = 1;
+
+console.log(number++); 
+console.log(++number);
+console.log(number--);
+console.log(--number);
