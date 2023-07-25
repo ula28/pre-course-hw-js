23983675426de56a7c12b26535b60b2ffd8af94a
ula28 <ula24499@gmail.com>
2023-07-25 17:03:24 +0200

    cor22

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -232,7 +232,7 @@
 
 let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом."
 
-switch (rule.length) {
+switch (true) {
 	case rule.length > 25:
       console.log('У меня 100 пудов все получится'); 
       break;
