d6e0392c48147d5cdf958b4fd39825952cf9e73c
ula28 <ula24499@gmail.com>
2023-07-27 13:08:04 +0200

    02-06.js

---
+++ lesson-02/06.js
@@ -0,0 +1,13 @@
+let passportWithAddress = {
+	name: "Petr",
+	surname: "Petrov",
+	address: {
+		 country: "USA",
+		 city: "LA"
+	}
+};
+let copyPassportWithAddress = {...passportWithAddress, address: {...passportWithAddress.address}};
+copyPassportWithAddress.address.city = "Bobryisk";
+
+console.log(passportWithAddress.address);
+console.log(copyPassportWithAddress.address);
