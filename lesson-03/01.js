130a2d58427768939f87f840a6fbe1fbc6550532
ula28 <ula24499@gmail.com>
2023-08-04 08:25:58 +0200

    03-01

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,10 +1,10 @@
 const colors = ['синий', 'черный', 'зеленый', 'красный', 'желтый', 'зеленый'];
 
 function createColorString () {
-	// фильтруем массив, чтобы оставить только требуемые цвета
+
 		const germanColors = colors.filter(color => color === 'черный' || color === 'красный' || color === 'желтый');
 
-		// обьединяем отфильтрованные цвета в строку с разделителем
+		
 		return germanColors.join('-');
 }
 console.log(createColorString ());
